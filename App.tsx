20744520011f6ba7714cf34a608ac8e3a4ca5289
Rahul Mendonca <rahul.mendonca@refineddata.com>
2022-04-13 11:13:26 -0400

    Adding typescript behaviour

--- App.tsx
+++ App.tsx
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            screen and then come back to 
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -83,7 +83,6 @@ const App: () => Node = () => {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
